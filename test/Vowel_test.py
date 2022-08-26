cab48d83fc774b913469c71b32bd703725c04d4a
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-08-26 17:37:54 -0400

    Add files via upload

---
+++ test/Vowel_test.py
@@ -0,0 +1,10 @@
+import Code as c
+def uppercase_vowel():
+    assert(c.vowel("A"))
+def lowercase_vowel():
+    assert(c.vowel("e"))
+def uppercase_consonant():
+    assert(c.vowel("b"))
+def capital_consonant():
+    assert(c.vowel("n"))
+
