ed2ef6f810cf8fccde701de70c993aac6ae6285e
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-08-26 19:04:46 -0400

    Update 2

--- code/Code.py
+++ code/Code.py
@@ -2,5 +2,7 @@ def vowel(c):
     x=['a','A','e','E','i','I','o','O','u','U']
     if(c in x):
         print(c," is vowel")
+        return 1
     else:
         print(c," is not vowel")
+        return 0
