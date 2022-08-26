80d00ea48e6d6b6d8e73cac1d008da13988237e0
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-08-26 18:48:31 -0400

    Update code.py

--- code/Code.py
+++ code/Code.py
@@ -1,9 +1,6 @@
-a=input("Enter the character:-")
 def vowel(c):
     x=['a','A','e','E','i','I','o','O','u','U']
     if(c in x):
         print(c," is vowel")
     else:
         print(c," is not vowel")
-
-vowel(a)
