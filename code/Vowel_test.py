589e99a67457c9a4d6b71af0966dad37f6f355c7
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-08-26 19:04:10 -0400

    Update Vowel_test.py

--- code/Vowel_test.py
+++ code/Vowel_test.py
@@ -1,10 +1,10 @@
 import Code as c
 def uppercase_vowel():
-    assert(c.vowel("A"))
+    assert(c.vowel("A")) == 1
 def lowercase_vowel():
-    assert(c.vowel("e"))
+    assert(c.vowel("e")) == 1
 def uppercase_consonant():
-    assert(c.vowel("b"))
+    assert(c.vowel("b")) == 0
 def capital_consonant():
-    assert(c.vowel("n"))
+    assert(c.vowel("n")) == 0
 
