f336a6835a6fc54e14be051a6fc02d2a10f75687
yagmurbbayraktar <yagmurbasakbayraktar@gmail.com>
2022-08-28 16:10:13 -0400

    Setup.py fix
    Setup.py name changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CSC-510-HW1',
+    name='vowelcheck',
     version='1.0.0',
     packages=find_packages(where="code"),
     package_dir={"": "code"},
