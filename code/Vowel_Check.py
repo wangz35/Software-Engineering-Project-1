17b0240efccf4a2233b7d373c3817dd770420c0b
yagmurbbayraktar <yagmurbasakbayraktar@gmail.com>
2022-08-28 16:00:35 -0400

    Setup.py added
    Setup.py added, code.py renamed, Vowel_Test moved under test file. Commit to test the setup.

---
+++ code/Vowel_Check.py
@@ -0,0 +1,8 @@
+def vowel(c):
+    x=['a','A','e','E','i','I','o','O','u','U']
+    if(c in x):
+        print(c," is vowel")
+        return 1
+    else:
+        print(c," is not vowel")
+        return 0
---
+++ test/Vowel_Test.py
@@ -0,0 +1,10 @@
+import Code as c
+def test_uppercase_vowel():
+    assert(c.vowel("A")) == 1
+def test_lowercase_vowel():
+    assert(c.vowel("e")) == 1
+def test_uppercase_consonant():
+    assert(c.vowel("b")) == 0
+def test_capital_consonant():
+    assert(c.vowel("n")) == 0
+
